b11dff30395c0705df3ad710585073ada93da385
Peter <peter.zmilczak@gmail.com>
2022-07-31 10:06:26 +0200

    first commit

---
+++ src/main/java/com/codecool/supertaxi/SuperTaxi/SuperTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.supertaxi.SuperTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperTaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/supertaxi/SuperTaxi/SuperTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.supertaxi.SuperTaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
